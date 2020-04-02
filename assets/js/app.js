741baa91496e0cd88a26b1b8a205da5e3439cc78
Saurin Patel <saurin@Saurins-Air.attlocal.net>
2020-04-02 18:31:31 -0400

    initial code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,18 @@
 // @TODO: YOUR CODE HERE!
+let svgWidth = 960;
+let svgHeight = 620;
+
+let margin = {
+    top: 20,
+    right: 40,
+    bottom: 200, 
+    left:100
+};
+// Chart height and width
+ let width = svgWidth - margin.left - margin.right;
+ let height = svgHeight - margin.top - margin.bottom;
+
+ let chart = d3
+ .select('#scatter')
+ .append("div")
+ ,classed("chart", true);
